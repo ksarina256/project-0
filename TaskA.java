9fb26356588afb4dd9a075446760658178b41a8f
Sarina Khara <ksarina256@gmail.com>
2025-01-26 16:36:08 -0800

    commit 3

--- TaskA.java
+++ TaskA.java
@@ -3,9 +3,6 @@
 // Assignment 2
 // 9/10/24
 
-// testing
-// hello
-
 import java.util.Scanner; // Importing Scanner class for user input
 
 // This program calculates the required ingredient amounts for making shortbread cookies based on user input
